027b5118fd947326292d2b48af1543e6a3edc7ee
jveiiga <jeferson.veiiga@icloud.com>
2025-04-24 00:22:36 -0300

    Configurando deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/api-github-avanti/', // Substitua <seu-repositorio> pelo nome do seu repositório
+});
